dad8d2dd01d57bcf570e16d624786de306c116e8
Mihuy1 <patrik.hyytiainen@protonmail.com>
2026-02-16 17:49:45 +0200

    Add a discover page with timeslots

--- src/Pages/Discover.jsx
+++ src/Pages/Discover.jsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from "react";
+import { getAllTimeSlots } from "../api/apiClient";
+import { TimeSlot } from "../components/Timeslot";
+
 function Discover() {
-    return (
-        <div>
-            <h1>Discover</h1>
-        </div>
-    );
+  const [timeslots, setTimeSlots] = useState([]);
+
+  useEffect(() => {
+    const fetchTimeSlots = async () => {
+      try {
+        const slots = await getAllTimeSlots();
+
+        console.log("Slots:", slots);
+
+        if (slots) setTimeSlots(slots);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTimeSlots();
+  }, []);
+
+  return (
+    <div className="discover-container">
+      <h1 className="discover-title">Discover Experiences</h1>
+      <div className="timeslots-grid">
+        {timeslots.map((slot) => (
+          <TimeSlot
+            key={slot.id}
+            id={slot.id}
+            host_id={slot.host_id}
+            type={slot.type}
+            start_time={slot.start_time}
+            end_time={slot.end_time}
+            res_status={slot.res_status}
+            description={slot.description}
+            city={slot.city}
+            latitude_deg={slot.latitude_deg}
+            longitude_deg={slot.longitude_deg}
+            activity_type={slot.activity_type}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Discover;
